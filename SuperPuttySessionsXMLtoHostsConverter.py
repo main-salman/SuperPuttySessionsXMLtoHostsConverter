4f92a687358341d2fa18f4f685cf2d3c15fca6bc
main-salman <salman.naqvi@gmail.com>
2022-04-09 22:48:00 -0400

    Create SuperPuttySessionsXMLtoHostsConverter.py

---
+++ SuperPuttySessionsXMLtoHostsConverter.py
@@ -0,0 +1,14 @@
+import xmltodict
+import pprint
+
+with open('sessions.xml', 'rU') as sessionsxml:
+    for line in sessionsxml:
+        try:
+            hosts_dict = xmltodict.parse(line)
+            session=dict(hosts_dict['SessionData'])
+            print(session['@Host'] + " " + (session["@SessionName"]).split(' ', 1)[0])
+            hosts = open("hosts.txt", "a")
+            hosts.write(session['@Host'] + " " + (session["@SessionName"]).split(' ', 1)[0]+"\n")
+            
+        except:
+hosts.close()
